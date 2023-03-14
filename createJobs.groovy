ee970ca058b4f217e1574be943b8d0fb6d8d3f1f
jcoro <john@coronite.net>
2023-03-14 13:36:41 -0700

    Added tutorial repo

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,7 +12,7 @@ pipelineJob('theme-park-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/jcoro/spring-boot-app.git'
+                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
                     }
                     branch 'master'
                 }
